39004648145b71decb406b4147561b5d620f2e2e
graffegiraffe <katerublevsk@icluod.com>
2025-05-28 01:25:32 +0300

    Add Actuator to the project. Add CustomInfoContributor for INFO endpoint

---
+++ src/main/java/by/rublevskaya/actuator/CustomInfoContributor.java
@@ -0,0 +1,24 @@
+package by.rublevskaya.actuator;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class CustomInfoContributor implements InfoContributor {
+
+    @Override
+    public void contribute(Info.Builder builder) {
+        builder
+                .withDetail("app", Map.of(
+                        "name", "Pet-project",
+                        "description", "Medical Assistant Management App",
+                        "version", "1.0.0"
+                ))
+                .withDetail("contact", Map.of(
+                        "email", "katerublevsk@icloud.com"
+                ));
+    }
+}
